070591dea035aeaf92dda3ead503c5652fca3737
arsalfimov <Alfimov.ars@gmail.com>
2022-11-08 10:30:40 +0300

    Add context interface

---
+++ Records.Application/Interfaces/IRecordsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Records.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Records.Application.Interfaces
+{
+    public interface IRecordsDbContext
+    {
+        DbSet<Record> Records { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
