57d20eb931d9f47d7e1bfe5665f0677c9ea95309
arsalfimov <Alfimov.ars@gmail.com>
2022-11-08 10:58:18 +0300

    Add persistence layer

---
+++ Records.Persistence/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Records.Persistence;
+
+namespace Records.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(RecordsDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ Records.Persistence/RecordsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Records.Application.Interfaces;
+using Records.Domain;
+using Records.Persistence.EntityTypeConfigurations;
+
+namespace Records.Persistence
+{
+    public class RecordsDbContext : DbContext, IRecordsDbContext
+    {
+        public DbSet<Record> Records { get; set; }
+
+        public RecordsDbContext(DbContextOptions<RecordsDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new RecordConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
