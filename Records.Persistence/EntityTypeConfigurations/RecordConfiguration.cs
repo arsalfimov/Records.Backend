887a9de701ea92472b3f7142529c550f0fa7c90e
arsalfimov <Alfimov.ars@gmail.com>
2022-11-08 10:21:55 +0300

    added entity configurations

---
+++ Records.Persistence/EntityTypeConfigurations/RecordConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Records.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Records.Persistence.EntityTypeConfigurations
+{
+    public class RecordConfiguration : IEntityTypeConfiguration<Record>
+    {
+        public void Configure(EntityTypeBuilder<Record> builder)
+        {
+            builder.HasKey(record => record.Id);
+            builder.HasIndex(record => record.Id).IsUnique();
+            builder.Property(record => record.Title).HasMaxLength(250);
+        }
+    }
+}
