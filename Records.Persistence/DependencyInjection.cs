8fae89496abc8faa1e82f53e88471bab8540de15
arsalfimov <Alfimov.ars@gmail.com>
2022-11-21 11:51:23 +0300

    Add DependencyInjection

---
+++ Records.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Records.Application.Interfaces;
+
+namespace Records.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<RecordsDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IRecordsDbContext>(provider =>
+                provider.GetService<RecordsDbContext>());
+            return services;
+        }
+    }
+}
